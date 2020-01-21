d387ad785f6cd96acbf8ccb86913e8f81c663eff
Layton <laytonb@hotmail.co.uk>
2020-01-21 20:10:37 +0000

    Issue Templates added, to do list added, minor format changes

--- Automatic Wild Shape.js
+++ Automatic Wild Shape.js
@@ -1,18 +1,4 @@
-/*
-README
-In order to prepare a beast for wild-shaping, complete this checklist:
-1) Make sure the beast has a player-uploaded default token.
-2) Make sure you're using a filled out Roll20 D&D 5e Character Sheet.
-3) ??? - If its not working with a default SRD monster sheet & blank player-uploaded default token then something has gone wrong. Hit me up at the link below.
-
-To override the class requirement of "Druid" and disable the CR limit set by character level:
-1) Create an attribute on the NPC or PC sheet.
-2) Name the attribute "aws_override".
-3) Set the attribute value to "1".
-If a PC is using the "aws_override" attribute the GM will be notified.
-
-Automatic Wild Shape written by Layton - https://app.roll20.net/users/1519557/layton
-*/
+// keep turn order for shape changed creature
 
 // Constants
 const AWS_name = "AWS";
